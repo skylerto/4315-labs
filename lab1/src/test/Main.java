236acc05e5868f23efcd99c15376ad1cce668cd8
Skyler Layne <skylerclayne@gmail.com>
2016-02-03 18:16:06 -0500

    Lab5
    Lab5 finished, notable fact -- `native_classpath` is used to set the listener we
    want/built!

--- lab1/src/test/Main.java
+++ lab1/src/test/Main.java
@@ -8,12 +8,13 @@ import org.junit.runner.notification.Failure;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		PrintStream output = System.out;
 		Result result = JUnitCore.runClasses(TestByte.class);
 		for (Failure failure : result.getFailures()) {
 			output.println(failure.getMessage());
 			output.println(failure.getException());
+            throw failure.getException();
 		}
 		if (result.wasSuccessful()) {
 			output.println("All tests passed.");
