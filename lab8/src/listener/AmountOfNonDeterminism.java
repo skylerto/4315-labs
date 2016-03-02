ad50dc31cb6b9af3356a6b7823000e1ac3421305
Skyler Layne <skylerclayne@gmail.com>
2016-03-02 14:53:46 -0500

    start lab8

---
+++ lab8/src/listener/AmountOfNonDeterminism.java
@@ -0,0 +1,13 @@
+import gov.nasa.jpf.JPFListener;
+import gov.nasa.jpf.ListenerAdapter;
+import gov.nasa.jpf.search.Search;
+
+public class AmountOfNonDeterminism extends ListenerAdapter implements JPFListener 
+{
+    /**
+     * Constructor.
+     */
+    public AmountOfNonDeterminism(){}
+
+}
+        
---
+++ lab8/src/main/Example.java
@@ -0,0 +1,16 @@
+package main;
+import java.util.Random;
+
+/**
+ * Example class given by prof.
+ */
+public class Example {
+        public static void main(String[] args) {
+            Random random = new Random();
+            if (random.nextBoolean()) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            } 
+        }
+}
